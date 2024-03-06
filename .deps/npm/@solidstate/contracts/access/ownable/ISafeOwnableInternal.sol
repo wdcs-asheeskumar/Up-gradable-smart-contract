15731c573643057d915372d89802f72a809e24ba
wdcs-asheeskumar <154244737+wdcs-asheeskumar@users.noreply.github.com>
2024-03-06 19:15:43 +0530

    Delete .deps directory

--- .deps/npm/@solidstate/contracts/access/ownable/ISafeOwnableInternal.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.18;
-
-import { IOwnableInternal } from './IOwnableInternal.sol';
-
-interface ISafeOwnableInternal is IOwnableInternal {
-    error SafeOwnable__NotNomineeOwner();
-}
